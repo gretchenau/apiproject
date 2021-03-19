ad17ee75e5dfcd0b5237531d5ed12334910bebb2
gretchenau <gretchenaussie@gmail.com>
2021-03-19 18:27:18 +0300

    k

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(_se8nm)0!u8=p@u3=1@z4ni_jazycfufxu0^gmk0=sn*_n66='
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
