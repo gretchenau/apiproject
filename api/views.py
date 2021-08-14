c4e66768450df2c02866f27ca89c776bede834eb
gretchenau <gretchenaussie@gmail.com>
2021-08-14 20:03:21 +0300

    serializers

--- api/views.py
+++ api/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from .models import Article
+from .serializers import ArticleSerializer
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
-def index(request):
-    return HttpResponse("It is working!")
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'GET':
+        data
